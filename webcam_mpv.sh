748eab5f00d1811e42093bfb63bd735f616a48f0
Yogi <zkhafoficial@gmail.com>
2026-07-25 13:57:18 -0300

    fix: wrong /dev/video

--- webcam_mpv.sh
+++ webcam_mpv.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-v4l2-ctl -d /dev/video1 --set-ctrl=brightness=150
+v4l2-ctl -d /dev/video0 --set-ctrl=brightness=150
 
-flatpak run io.mpv.Mpv av://v4l2:/dev/video1 \
+flatpak run io.mpv.Mpv av://v4l2:/dev/video0 \
     --demuxer-lavf-o=video_size=1280x720,framerate=30,input_format=mjpeg \
     --profile=low-latency \
     --untimed \
     --no-osc --no-osd-bar --no-input-default-bindings \
     --border=no \
-    --ontop \
     --vf=crop=400:400 \
     --video-aspect-override=no \
     --autofit=360x360 \
--- webcam_zoom.sh
+++ webcam_zoom.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-v4l2-ctl -d /dev/video1 --set-ctrl=brightness=150
+v4l2-ctl -d /dev/video0 --set-ctrl=brightness=150
 
 gst-launch-1.0 \
-    v4l2src device=/dev/video1 ! \
+    v4l2src device=/dev/video0 ! \
     image/jpeg,width=1280,height=720,framerate=30/1 ! \
     jpegdec ! \
     videocrop top=160 bottom=160 left=440 right=440 ! \
